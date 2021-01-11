d53cb85816c3423c5f3823657975fa76a078759d
vahid <vahidmostofi@outlook.com>
2021-01-10 20:14:46 -0700

    WIP

--- internal/restime/restime.go
+++ internal/restime/restime.go
@@ -1,17 +1,23 @@
 package restime
 
 import (
+	"context"
+	"github.com/influxdata/influxdb-client-go/v2/api"
 	"github.com/montanaflynn/stats"
 	"github.com/pkg/errors"
+	"github.com/vahidmostofi/acfg/internal/dataaccess"
+	"log"
+	"strconv"
+	"strings"
 )
 
 // ResponseTimeAggregator uses some functionality to gather response times based on some functionality
 // between startTime and finishTime. The filters might be used to add selection functionality.
-// if startTime is less than 0, it should be replaced with 0
-// if finishTime is less than 0, it should be replaced with current time
+// if startTime is less than 0, it should be replaced with 0; time.Unix()
+// if finishTime is less than 0, it should be replaced with current time time.Unix()
 // if filters is null, there operation should be done without any filtering.
 type ResponseTimeAggregator interface {
-	GetResponseTimes(startTime, finishTime float64, filters map[string]interface{}) (*ResponseTimes, error)
+	GetResponseTimes(startTime, finishTime int64, filters map[string]interface{}) (*ResponseTimes, error)
 }
 
 // ResponseTimes is a named type for []float64 with helper functions
@@ -51,4 +57,57 @@ func (rts *ResponseTimes) GetPercentile(p float64) (float64, error) {
 		return 0, errors.Wrap(err, "computing percentile failed on response times")
 	}
 	return p, nil
+}
+
+// InfluxDBRTA gets response times from influxdb
+type InfluxDBRTA struct{
+	qAPI api.QueryAPI
+	ctx context.Context
+	cnF context.CancelFunc
+	org string
+	bucket string
+}
+
+func NewInfluxDBRTA(url, token, organization, bucket string) *InfluxDBRTA{
+	ctx, cnF := context.WithCancel(context.Background())
+	i := &InfluxDBRTA{ctx: ctx, cnF: cnF, org: organization, bucket: bucket}
+	i.qAPI = dataaccess.GetNewClientAndQueryAPI(url, token, organization)
+
+	return i
+}
+
+func (i *InfluxDBRTA) GetResponseTimes(startTime, finishTime int64, filters map[string]interface{})  (*ResponseTimes, error){
+	query := `
+from(bucket: "$BUCKET_NAME")
+  |> range(start: $START_TIME, stop: $FINISH_TIME)
+  |> filter(fn: (r) => r["_measurement"] == "request_info" and r._field == "ust" $CONDITIONS)
+  |> group()
+  |> keep(columns: ["_time", "_value"])
+`
+	query = strings.Replace(query, "$BUCKET_NAME", i.bucket,-1 )
+	query = strings.Replace(query, "$START_TIME", strconv.FormatInt(startTime, 10),-1 )
+	query = strings.Replace(query, "$FINISH_TIME", strconv.FormatInt(finishTime, 10),-1 )
+
+	conditions := ""
+	if httpMethod, ok := filters["HTTP_METHOD"]; ok{
+		if conditions == ""{
+			conditions = " and "
+		}
+		conditions += "r.method == \"\"" + httpMethod.(string) + "\"\""
+	}
+	if uriRegex, ok := filters["URI_REGEX"]; ok{
+		conditions += " and r.uri =~ /" + uriRegex.(string) + "*/"
+	}
+	query = strings.Replace(query, "$CONDITIONS", conditions, -1)
+
+	log.Println("getting response times from influxdb with query:\n" + query)
+
+	_, values, err := dataaccess.QuerySingleTable(i.qAPI, i.ctx, query, "_value")
+	if err != nil{
+		return nil, errors.Wrap(err, "error getting response times from influxdb using:\n" + query)
+	}
+
+	r := ResponseTimes(values)
+
+	return &r, nil
 }
--- internal/restime/restime_test.go
+++ internal/restime/restime_test.go
@@ -1,8 +1,11 @@
 package restime
 
 import (
+	"context"
 	"fmt"
+	"github.com/influxdata/influxdb-client-go/v2/api"
 	"math"
+	"reflect"
 	"testing"
 )
 
@@ -38,5 +41,48 @@ func TestSimpleAggregatorStats(t *testing.T) {
 	}
 	if math.Abs(p90-2) > 1e-4 {
 		t.Fail()
+	}
+}
+
+func TestInfluxDBRTA_GetResponseTimes(t *testing.T) {
+	type fields struct {
+		qAPI   api.QueryAPI
+		ctx    context.Context
+		cnF    context.CancelFunc
+		org    string
+		bucket string
+	}
+	type args struct {
+		startTime  int64
+		finishTime int64
+		filters    map[string]interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *ResponseTimes
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			i := &InfluxDBRTA{
+				qAPI:   tt.fields.qAPI,
+				ctx:    tt.fields.ctx,
+				cnF:    tt.fields.cnF,
+				org:    tt.fields.org,
+				bucket: tt.fields.bucket,
+			}
+			got, err := i.GetResponseTimes(tt.args.startTime, tt.args.finishTime, tt.args.filters)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetResponseTimes() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetResponseTimes() got = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
