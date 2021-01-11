de67a2b7da48f88908123478acdf3e44474c6325
vahid <vahidmostofi@outlook.com>
2021-01-10 20:14:46 -0700

    WIP

--- internal/dataaccess/influxdb.go
+++ internal/dataaccess/influxdb.go
@@ -1,12 +1,42 @@
 package dataaccess
 
 import (
+	"context"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
+	"github.com/pkg/errors"
+	"time"
 )
 
-// GetNewClientAndQueryAPI returns new ...
-func GetNewClientAndQueryAPI(url, token string) (influxdb2.Client, error) {
-	client := influxdb2.NewClient(url, token)
+// GetNewQueryAPI returns new QueryAPI
+func GetNewClientAndQueryAPI(url, token, organization string) api.QueryAPI {
+	client := getNewClient(url, token)
+	queryAPI := client.QueryAPI(organization)
+	return queryAPI
+}
 
-	return client, nil
+func QuerySingleTable(queryAPI api.QueryAPI, ctx context.Context, q, valueName string) ([]time.Time,[]float64, error){
+	res, err := queryAPI.Query(ctx, q)
+	if err != nil{
+		return nil,nil,errors.Wrap(err, "error running query: " +  q)
+	}
+
+	times := make([]time.Time,0)
+	values := make([]float64,0)
+
+	for res.Next() {
+		t := res.Record().Time()
+		v := res.Record().ValueByKey(valueName)
+		if v != nil{
+			times = append(times, t)
+			values = append(values, v.(float64))
+		}
+	}
+
+	return times, values, nil
+}
+
+// getNewClient returns new Client
+func getNewClient(url, token string) influxdb2.Client {
+	return influxdb2.NewClient(url, token)
 }
