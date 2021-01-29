a75c7c4606b3e92bf0da5fe48321cb4e0a6778b6
vahid <vahid4m@gmail.com>
2021-01-29 13:05:30 -0700

    final touch for teastore-local

--- run_teastore.sh
+++ run_teastore.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+go build -o acfg
+set -e # stop on first error
+set -a # automatically export all variables
 source dev.env
-go run main.go --config sample-configs/teastore.yml --name teastore-test cput --indicator mean --initcpu 250 --initmem 2048 --threshold 45
+set +a
+./acfg autoconfig --config sample-configs/teastore.yml --name teastore-test cput --indicator mean --initcpu 250 --initmem 2048 --threshold 45
