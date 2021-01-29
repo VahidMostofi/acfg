c09f5758ec2538fc722fff7ee36d710cf243b460
vahid <vahid4m@gmail.com>
2021-01-29 08:58:34 -0700

    set log level using envs

--- main.go
+++ main.go
@@ -2,7 +2,24 @@ package main
 
 import (
 	"github.com/vahidmostofi/acfg/cmd"
+	"github.com/sirupsen/logrus"
+	"os"
 )
+
+func init() {
+    lvl, ok := os.LookupEnv("LOG_LEVEL")
+    // LOG_LEVEL not set, let's default to debug
+    if !ok {
+        lvl = "debug"
+    }
+    // parse string, this is built-in feature of logrus
+    ll, err := logrus.ParseLevel(lvl)
+    if err != nil {
+        ll = logrus.DebugLevel
+    }
+    // set global log level
+    logrus.SetLevel(ll)
+}
 
 func main() {
 	cmd.Execute()
