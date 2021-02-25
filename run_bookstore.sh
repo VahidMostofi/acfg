46d1923422b1512e49d51e96f4ec3190c231fb20
vahid <vahid4m@gmail.com>
2021-02-24 18:19:14 -0700

    run file for bookstore with mobo and bnv1

--- run_bookstore.sh
+++ run_bookstore.sh
@@ -4,6 +4,13 @@ set -e # stop on first error
 set -a # automatically export all variables
 source dev.env
 set +a
-# ./acfg autoconfig --config sample-configs/bookstore.yml --name bookstore-test cput --indicator mean --initcpu 500 --initmem 512 --threshold 45
+# ./acfg autoconfig --config sample-configs/bookstore.yml --name bookstore-test cput --indicator mean --initcpu 500 --initmem 256 --threshold 45
 
-./acfg autoconfig --config sample-configs/bookstore.yml --name bookstore-test bnv2 --initialdelta 2000 --initcpu 500 --initmem 512 --maxcpuperreplica 500 --mincpu 500 --mindelta 500
+./acfg autoconfig --config sample-configs/bookstore.yml --name bookstore-bnv2 bnv2 --initialdelta 2000 --initcpu 500 --initmem 256 --maxcpuperreplica 500 --mincpu 500 --mindelta 500
+
+# MOBO
+PYTHON_PATH=/home/vahid/.pyenv/versions/3.6.13/bin/python3
+SCRIPT_PATH=$PWD/pythons/mobo-bookstore.py
+echo $SCRIPT_PATH
+echo $PYTHON_PATH
+./acfg autoconfig --config sample-configs/bookstore.yml --name bookstore-mobo stdin --initcpu 500 --initmem 256 --pythonpath $PYTHON_PATH --scriptpath $SCRIPT_PATH
